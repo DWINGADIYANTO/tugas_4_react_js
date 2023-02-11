8bec5d937bc7dacae8d029a42508808704953ef7
Dwi Ngadiyanto <dwingadiyanto879@gmail.com>
2023-02-11 16:47:44 +0700

    Tugas 4 React.js
    Tugas ini kami kumpulkan untuk Niomic

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ListMakanan from './ListData/ListMakanan';
+import MenuMakanan from './Page/MenuMakanan';
+
+function App() {
+  return (
+    <div>
+      <ListMakanan />
+      <MenuMakanan />
+    </div>
+  );
+}
+export default App;
